88847e9a39625e0f5025abfbfe7f616b7d933349
gadgetguycj <clauer@nyit.edu>
2018-11-06 20:44:14 -0500

    Added HW5 Part 1

--- Programming2/src/HW5/Compute.java
+++ Programming2/src/HW5/Compute.java
@@ -1,10 +1,30 @@
 package HW5;
 
+import java.util.Scanner;
+
 public class Compute {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Chase Lauer
+		Scanner read = new Scanner(System.in);
+		int x=0, y=0;
+		
+		try {
+			System.out.println("Please input integer #1"); x = read.nextInt();
+			System.out.println("Please input integer #2"); y = read.nextInt();
+			
+			
+		} catch (Exception e){
+			System.err.println("Error, please enter values again");
 
+			
+		}finally {
+			System.out.println("Remainder: "+ (x%y));
+		}
+
+		
+
+		
 	}
 
 }
